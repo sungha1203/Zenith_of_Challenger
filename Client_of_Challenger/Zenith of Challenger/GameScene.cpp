4b54a94e0ecbc4cacdf5d144332888f93a8fceee
sungha1203 <hih20553@naver.com>
2025-04-17 20:12:37 +0900

    스레드 코어 개수 변경

--- Client_of_Challenger/Zenith of Challenger/GameScene.cpp
+++ Client_of_Challenger/Zenith of Challenger/GameScene.cpp
@@ -335,8 +335,8 @@ void GameScene::BuildObjects(const ComPtr<ID3D12Device>& device)
 		auto player = make_shared<Player>(device);
 
 		// [4] 위치 및 스케일 설정
-		player->SetPosition(XMFLOAT3{ -185.f, 53.f, 177.f });
-		//player->SetPosition(gGameFramework->g_pos);
+		//player->SetPosition(XMFLOAT3{ -185.f, 53.f, 177.f });
+		player->SetPosition(gGameFramework->g_pos);
 
 		// [5] FBX 메시 전부 등록
 		for (int i = 0; i < meshes.size(); ++i)
