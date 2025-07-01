c0554d4013419138e0a3738ba965e6375c639f00
zyhwan <145274494+zyhwan@users.noreply.github.com>
2025-07-01 21:28:46 +0900

    터레인 그림자 완

--- Client_of_Challenger/Zenith of Challenger/Terrain.hlsl
+++ Client_of_Challenger/Zenith of Challenger/Terrain.hlsl
@@ -12,14 +12,23 @@ struct DETAIL_PIXEL_INPUT
     float4 position : SV_POSITION;
     float2 uv0 : TEXCOORD0;
     float2 uv1 : TEXCOORD1;
+    float4 shadowPos : TEXCOORD2;
 };
 
 DETAIL_PIXEL_INPUT DETAIL_VERTEX(DETAIL_VERTEX_INPUT input)
 {
     DETAIL_PIXEL_INPUT output;
-    output.position = mul(float4(input.position, 1.0f), g_worldMatrix);
-    output.position = mul(output.position, g_viewMatrix);
+
+    float4 worldPos = mul(float4(input.position, 1.0f), g_worldMatrix);
+
+    // 뷰-프로젝션 변환 (화면 위치)
+    output.position = mul(worldPos, g_viewMatrix);
     output.position = mul(output.position, g_projectionMatrix);
+
+    // 그림자 좌표는 shadowMat으로 한 번에 변환 (View * Proj * Bias)
+    output.shadowPos = mul(worldPos, shadowMat);
+
+    // 텍스처 좌표 복사
     output.uv0 = input.uv0;
     output.uv1 = input.uv1;
 
@@ -28,8 +37,15 @@ DETAIL_PIXEL_INPUT DETAIL_VERTEX(DETAIL_VERTEX_INPUT input)
 
 float4 DETAIL_PIXEL(DETAIL_PIXEL_INPUT input) : SV_TARGET
 {
-    //return g_texture[0].Sample(g_sampler, input.uv0); // 하나의 텍스처만 샘플링
- 
-    return lerp(g_texture[0].Sample(g_sampler, input.uv0),
-    g_texture[1].Sample(g_sampler, input.uv1), 0.5f);
+    // 텍스처 블렌딩
+    float4 texColor = lerp(
+        g_texture[0].Sample(g_sampler, input.uv0),
+        g_texture[1].Sample(g_sampler, input.uv1),
+        0.5f
+    );
+
+    // ComputeShadowFactor는 내부에서 .xyz/w, bias, 0~1 변환 모두 수행
+    float shadow = ComputeShadowFactor(input.shadowPos.xyz);
+
+    return texColor * shadow;
 }
